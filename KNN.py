27cbf1b826d09e2f6143febceac1d4b97a79a940
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2019-01-24 00:11:07 -0330

    Add files via upload

--- KNN.py
+++ KNN.py
@@ -1,37 +1,24 @@
 import pandas as pd
-import numpy as np
-import math as mt
 import sys
-import csv
-import operator
-#sys.argv[1] = 'train.csv'
 
+# Load the training set and testing set
+sys.argv[1] = 'train.csv'
 df_train = pd.read_csv('train.csv')
 input_data_train = df_train.astype(float).values.tolist()
 full_train_data = df_train.astype(float).values.tolist()
 for i in full_train_data:
     del i[-1]
-a = full_train_data
-#a1 = a[0]
-#flat_list = [item for sublist in a for item in sublist]
-#print(flat_list)
-#print(a1)
-#print(len(a1))
-#print(len(a))
+a = full_train_data         # Remove the "Class" Column from the training set.
+
+sys.argv[2] = 'test.csv'
 
 df_test = pd.read_csv('test.csv')
 full_test_data = df_test.astype(float).values.tolist ()
 b = full_test_data
-#b1 = b [0]
-#print(b1)
-#print(len(b1))
-#print(b)
 
 
 # Euclidean Distance
 
-
-#################################################################
 def euclidean(v1, v2):
     return sum((p-q)**2 for p, q in zip(v1, v2)) ** .5
 
@@ -39,86 +26,80 @@ d2 = []
 for i in b:
     foo = [euclidean(i, j) for j in a]
     d2.append(foo)
-#print('Euclidean Disntance: ', d2[0])
 
-#print('Length: ', len(d2))
-print('Each List: ', len(d2[0]))
+#Merge the euclidean distance into training data set
 included_data = []
+row = int(input("Please Enter The Row Number of the Testing Dataset: " + "(0 to 9)"))
 for counter in range(204):
-    input_data_train[counter].append(d2[0][counter])
+    input_data_train[counter].append(d2[row][counter])
     included_data.append(input_data_train[counter])
-    #print(a[counter])
-#print(included_data)
 from operator import itemgetter
-sorted_data = sorted(included_data, key=itemgetter(10))
-print(len(included_data))
-print(sorted_data)
+sorted_data = sorted(included_data, key=itemgetter(10)) # Sorted the Euclidean distance in ascending order
 
+try:
+    k = int(input('Please put the value for K: ')) # Take the value of K and set the default value of K=3
+except ValueError:
+    k = 3
+
+# Make a sorted list with "Class" & Euclidean Distance
 knn_list = []
-k = 3
 for elements in range(k):
     knn = sorted_data[elements][9:]
     knn_list.append(knn)
-print(knn_list)
-
-from collections import Counter
-data_counter_1 = 0
-data_counter_2 = 0
-data_counter_3 = 0
-data_counter_5 = 0
-data_counter_6 = 0
-data_counter_7 = 0
-for count in range (k):
-    counted_item = knn_list[count][0]
-    if counted_item == 1:
-        data_counter_1 = data_counter_1 + 1
-    elif counted_item == 2:
-        data_counter_2 = data_counter_2 + 1
-    elif counted_item == 3:
-        data_counter_3 = data_counter_3 + 1
-    elif counted_item == 5:
-        data_counter_5 = data_counter_5 + 1
-    elif counted_item == 6:
-        data_counter_6 = data_counter_6 + 1
-    elif counted_item == 7:
-        data_counter_7 == data_counter_7 + 1
-    print(counted_item)
-data_counter_list = [data_counter_1,data_counter_2,data_counter_3,data_counter_5,data_counter_6,data_counter_7]
-print(data_counter_list)
-if data_counter_1 > 0:
-    print('No. Of 1s: ', data_counter_1)
-if data_counter_2 > 0:
-    print('No. Of 2s: ', data_counter_2)
-if data_counter_3 > 0:
-    print('No. Of 3s: ', data_counter_3)
-if data_counter_5 > 0:
-    print('No. Of 5s: ', data_counter_5)
-if data_counter_6 > 0:
-    print('No. Of 6s: ', data_counter_6)
-if data_counter_7 > 0:
-    print('No. Of 7s: ', data_counter_7)
-
-maximum_repeated = max(data_counter_list)
-if data_counter_1 == maximum_repeated:
-    print('Number of Class- 1: ', maximum_repeated)
-elif data_counter_2 == maximum_repeated:
-    print('Number of Class- 2: ', maximum_repeated)
-elif data_counter_3 == maximum_repeated:
-    print('Number of Class- 3: ', maximum_repeated)
-elif data_counter_5 == maximum_repeated:
-    print('Number of Class- 5: ', maximum_repeated)
-elif data_counter_6 == maximum_repeated:
-    print('Number of Class- 6: ', maximum_repeated)
-elif data_counter_7 == maximum_repeated:
-    print('Number of Class- 7: ', maximum_repeated)
-print('')
-
-accuracy_knn = (maximum_repeated*100)/k
-print('Accuracy of KNN Calculation: ',accuracy_knn,'%')
 
 
+#Calculate the "Class" based on the sorted list
+m = 0
+n = 0
+o = 0
+p = 0
+q = 0
+r = 0
+for_test = knn_list
+print('Class with Euclidean Distance for Row ', row, ' is: ', for_test)
+print('And The length is ', len(knn_list))
 
+for test in range(len(for_test)):
+    z = for_test[test][0]
+    if for_test[test][0] == 1:
+        m += 1
+        #print(m)
+        #print('Class: "1"')
+    elif for_test[test][0] == 2:
+        n += 1
+        #print(n)
+        #print('Class: "2"')
+    elif for_test[test][0] == 3:
+        o += 1
+        #print(o)
+        #print('Class: "3"')
+    elif for_test[test][0] == 5:
+        p += 1
+        #print('Class: "5"')
+        #print(p)
+    elif for_test[test][0] == 6:
+        q +=1
+        #print(q)
+        #print('Class: "6"')
+    elif for_test[test][0] == 7:
+        #print('Class: "7"', r)
+        r +=1
+s = [m,n,o,p,q,r]
+s_max = max(s) # Calculate the maximum iterated class
+if s_max == m:
+    print('Your Dataset Is in Class "1"')
+elif s_max == n:
+    print('Your Dataset Is in Class "2"')
+elif s_max == o:
+    print('Your Dataset Is in Class "3"')
+elif s_max == p:
+    print('Your Dataset Is in Class "5"')
+elif s_max == q:
+    print('Your Dataset Is in Class "6"')
+elif s_max == r:
+    print('Your Dataset Is in Class "7"')
 
- #########################################################
-
+#Calculate the accuracy of value of KNN
+acc_knn = (s_max*100)/k
+print('Estimated conditional probability of the predicted class: ',acc_knn,'%')
 
