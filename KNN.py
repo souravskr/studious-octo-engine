ea800f72b2affd6c3bdbfa55956156c84391064e
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2019-01-24 23:02:07 -0330

    Add files via upload

--- KNN.py
+++ KNN.py
@@ -1,20 +1,22 @@
-import pandas as pd
 import sys
+import pandas as pd
+from operator import itemgetter
+
 
 # Load the training set and testing set
-sys.argv[1] = 'train.csv'
-df_train = pd.read_csv('train.csv')
+sys.argv[1] = 'TrainingData_A1.tsv'
+df_train = pd.read_csv('TrainingData_A1.tsv', sep='\t')
 input_data_train = df_train.astype(float).values.tolist()
 full_train_data = df_train.astype(float).values.tolist()
 for i in full_train_data:
-    del i[-1]
-a = full_train_data         # Remove the "Class" Column from the training set.
+    del i[-1]                   # Remove the "Class" Column from the training set.
 
-sys.argv[2] = 'test.csv'
 
-df_test = pd.read_csv('test.csv')
+sys.argv[2] = 'TestData_A1.tsv'
+
+df_test = pd.read_csv('TestData_A1.tsv', sep='\t')
 full_test_data = df_test.astype(float).values.tolist ()
-b = full_test_data
+
 
 
 # Euclidean Distance
@@ -22,24 +24,23 @@ b = full_test_data
 def euclidean(v1, v2):
     return sum((p-q)**2 for p, q in zip(v1, v2)) ** .5
 
-d2 = []
-for i in b:
-    foo = [euclidean(i, j) for j in a]
-    d2.append(foo)
+distance_list = []
+for i in full_test_data:
+    distance = [euclidean(i, j) for j in full_train_data]
+    distance_list.append(distance)
 
 #Merge the euclidean distance into training data set
 included_data = []
 row = int(input("Please Enter The Row Number of the Testing Dataset: " + "(0 to 9)"))
 for counter in range(204):
-    input_data_train[counter].append(d2[row][counter])
+    input_data_train[counter].append(distance_list[row][counter])
     included_data.append(input_data_train[counter])
+    #print(included_data)
 from operator import itemgetter
 sorted_data = sorted(included_data, key=itemgetter(10)) # Sorted the Euclidean distance in ascending order
+print(sorted_data)
 
-try:
-    k = int(input('Please put the value for K: ')) # Take the value of K and set the default value of K=3
-except ValueError:
-    k = 3
+k = int(sys.argv[3]) if len(sys.argv) >= 4 else int(3)
 
 # Make a sorted list with "Class" & Euclidean Distance
 knn_list = []
@@ -55,34 +56,22 @@ o = 0
 p = 0
 q = 0
 r = 0
-for_test = knn_list
-print('Class with Euclidean Distance for Row ', row, ' is: ', for_test)
-print('And The length is ', len(knn_list))
+print('Nearest Neighbours are in row (', row, ') is: ', knn_list)
+print('For K =', k)
 
-for test in range(len(for_test)):
-    z = for_test[test][0]
-    if for_test[test][0] == 1:
+for test in range(len(knn_list)):
+    z = knn_list[test][0]
+    if knn_list[test][0] == 1:
         m += 1
-        #print(m)
-        #print('Class: "1"')
-    elif for_test[test][0] == 2:
+    elif knn_list[test][0] == 2:
         n += 1
-        #print(n)
-        #print('Class: "2"')
-    elif for_test[test][0] == 3:
+    elif knn_list[test][0] == 3:
         o += 1
-        #print(o)
-        #print('Class: "3"')
-    elif for_test[test][0] == 5:
+    elif knn_list[test][0] == 5:
         p += 1
-        #print('Class: "5"')
-        #print(p)
-    elif for_test[test][0] == 6:
+    elif knn_list[test][0] == 6:
         q +=1
-        #print(q)
-        #print('Class: "6"')
-    elif for_test[test][0] == 7:
-        #print('Class: "7"', r)
+    elif knn_list[test][0] == 7:
         r +=1
 s = [m,n,o,p,q,r]
 s_max = max(s) # Calculate the maximum iterated class
@@ -100,6 +89,6 @@ elif s_max == r:
     print('Your Dataset Is in Class "7"')
 
 #Calculate the accuracy of value of KNN
-acc_knn = (s_max*100)/k
+acc_knn = (s_max*100)/len(knn_list)
 print('Estimated conditional probability of the predicted class: ',acc_knn,'%')
 
